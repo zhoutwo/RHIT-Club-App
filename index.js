6d067d2763540ee009bbb0a15ccb288fec6fbfa7
zhoutwo <7986384+zhoutwo@users.noreply.github.com>
2017-01-27 17:58:42 -0500

    Http 2 server intial setup (#1)

---
+++ index.js
@@ -0,0 +1,29 @@
+const port = process.env.port || 3000;
+const spdy = require('spdy');
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+
+const app = express();
+
+const options = {
+    key: fs.readFileSync(path.join(__dirname, '/cert/server.key')),
+    cert:  fs.readFileSync(path.join(__dirname, '/cert/server.crt'))
+};
+
+app.get('*', (req, res) => {
+    res
+      .status(200)
+      .json({message: 'ok'})
+});
+
+spdy
+  .createServer(options, app)
+  .listen(port, (error) => {
+    if (error) {
+      console.error(error);
+      return process.exit(1);
+    } else {
+      console.log(`Server running on port ${port}`);
+    }
+  });
